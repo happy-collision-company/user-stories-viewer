c701d3751ab12fafcc99ff076d7e3239edd700a1
Don Denton <don@happycollision.com>
2017-12-03 18:52:40 -0600

    let's not do semicolons, mkay?

--- src/ui/components/UserStoriesViewer/component.ts
+++ src/ui/components/UserStoriesViewer/component.ts
@@ -1,7 +1,7 @@
-import Component, {tracked} from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component'
 
 export default class UserStoriesViewer extends Component {
-  @tracked rawYaml = '';
+  @tracked rawYaml = ''
 
   async didInsertElement() {
     this.rawYaml = await fetch('./user-stories.yml')
