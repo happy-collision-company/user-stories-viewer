10ebd682224f1c219c52436f3c6fea839595a078
Don Denton <don@happycollision.com>
2017-12-03 19:50:37 -0600

    parse the yaml and display it

--- src/ui/components/UserStoriesViewer/component.ts
+++ src/ui/components/UserStoriesViewer/component.ts
@@ -1,10 +1,14 @@
 import Component, { tracked } from '@glimmer/component'
+import yaml from 'js-yaml'
 
 export default class UserStoriesViewer extends Component {
-  @tracked rawYaml = ''
+  @tracked userStories = []
 
   async didInsertElement() {
-    this.rawYaml = await fetch('./user-stories.yml')
+    const rawYaml = await fetch('./user-stories.yml')
       .then(response => response.text())
+
+    this.userStories = yaml.safeLoad(rawYaml);
+    console.log(this.userStories)
   }
 }
