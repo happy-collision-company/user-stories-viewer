472bf5f82bde12998dc9036b0305d88f1493af56
Don Denton <don@happycollision.com>
2017-12-03 18:44:21 -0600

    get raw yaml from file into component
    Using subset of actual user stories as starter data

--- src/ui/components/UserStoriesViewer/component.ts
+++ src/ui/components/UserStoriesViewer/component.ts
@@ -1,5 +1,10 @@
-import Component from '@glimmer/component';
+import Component, {tracked} from '@glimmer/component';
 
 export default class UserStoriesViewer extends Component {
+  @tracked rawYaml = '';
 
+  async didInsertElement() {
+    this.rawYaml = await fetch('./user-stories.yml')
+      .then(response => response.text())
+  }
 }
