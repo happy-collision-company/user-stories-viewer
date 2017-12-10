ca943bf6e50039473b1651bb9f6ce4a63c5540d2
Don Denton <don@happycollision.com>
2017-12-09 20:09:51 -0600

    styling

--- src/ui/components/Story/component.ts
+++ src/ui/components/Story/component.ts
@@ -1,4 +1,19 @@
 import Component from '@glimmer/component';
 
 export default class Story extends Component {
+  a = this.roleBeginsWithVowel() ? 'an' : 'a';
+  period = this.reasonEndingPunctuation();
+
+  roleBeginsWithVowel(): boolean {
+    return ['a','e','i','o','u']
+      .indexOf(this.args.story.as_a.toLowerCase()[0]) > -1;
+  }
+
+  reasonEndingPunctuation() {
+    if (this.args.story.so_that.match(/\W$/)) {
+      return '';
+    } else {
+      return '.';
+    }
+  }
 }
